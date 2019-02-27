ba062b4ca7e31445bafa7a6d4fed5004e434a32f
webben <wspwebben@gmail.com>
2019-02-27 13:46:50 +0700

    updated interface

--- src/index.js
+++ src/index.js
@@ -3,15 +3,26 @@ import Observer from './observer.js';
 const observer = new Observer();
 
 const observerBlock = document.querySelector('.observer');
-const input = observerBlock.querySelector('.observer__input');
-const resultPlus = observerBlock.querySelector('.observer__result--plus');
-const resultMultiply = observerBlock.querySelector('.observer__result--multiply');
-const resultMod = observerBlock.querySelector('.observer__result--mod');
+const updateButton = observerBlock.querySelector('.js-update');
+const addButton = observerBlock.querySelector('.js-add');
 
-observer.subscribe(data => resultPlus.textContent = +data + 404);
-observer.subscribe(data => resultMultiply.textContent = data * 2);
-observer.subscribe(data => resultMod.textContent = data % 42);
+function addSubscriber() {
+  const template = document.querySelector('#subscriber');
+  const subscribers = observerBlock.querySelector('.js-subscribers');
+  const clone = template.content.cloneNode(true);
+  clone.querySelector('.js-remove').addEventListener('click', removeSubscriber);
+  subscribers.appendChild(clone);
 
-input.addEventListener('input', function() {
-    observer.notify(this.value);
+}
+
+function removeSubscriber() {
+  const subscriberBlock = this.closest('.js-sub');
+  this.removeEventListener('click', removeSubscriber);
+  subscriberBlock.parentNode.removeChild(subscriberBlock);
+}
+
+updateButton.addEventListener('click', function() {
+  observer.notify(this.value);
 });
+
+addButton.addEventListener('click', addSubscriber);
