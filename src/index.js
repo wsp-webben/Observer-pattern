1967ca1b1dd3c0f71ca0f7fb62d2e687a33fb162
webben <wspwebben@gmail.com>
2019-02-27 13:56:20 +0700

    add/remove subscribers build

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import Observer from './observer.js';
 const observer = new Observer();
 
 const observerBlock = document.querySelector('.observer');
+const updateInput = observerBlock.querySelector('.js-update-data');
 const updateButton = observerBlock.querySelector('.js-update');
 const addButton = observerBlock.querySelector('.js-add');
 
@@ -10,9 +11,13 @@ function addSubscriber() {
   const template = document.querySelector('#subscriber');
   const subscribers = observerBlock.querySelector('.js-subscribers');
   const clone = template.content.cloneNode(true);
-  clone.querySelector('.js-remove').addEventListener('click', removeSubscriber);
+  
+  const removeButton = clone.querySelector('.js-remove')
+  removeButton.addEventListener('click', removeSubscriber);
+  const dataField = clone.querySelector('.js-data');
+  observer.subscribe(data => dataField.textContent = data);
+  
   subscribers.appendChild(clone);
-
 }
 
 function removeSubscriber() {
@@ -22,7 +27,7 @@ function removeSubscriber() {
 }
 
 updateButton.addEventListener('click', function() {
-  observer.notify(this.value);
+  observer.notify(updateInput.value);
 });
 
 addButton.addEventListener('click', addSubscriber);
